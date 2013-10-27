214b44ccb5cefb5eb072be4f5667945e86e72910
tmgarcia <tclustuff@gmail.com>
2013-10-27 01:38:07 +0000

    Tabs 1 2 and 3 tentatively completed
    git-svn-id: https://svn.neumont.edu:8443/svn/taylorgarciarepspot@2 9283bf75-a890-104e-bf60-28af5b2e9440

---
+++ GameSkeleton/GameSkeleton/GameSolution/Engine/Vector2D.h
@@ -0,0 +1,70 @@
+#include "Math.h" 
+#ifndef VECTOR2D_H
+#define VECTOR2D_H
+struct Vector2D
+{
+	float x;
+	float y;
+	Vector2D(float x = 0, float y = 0) : x(x), y(y) {}
+	friend Vector2D operator*(float scalar, const Vector2D& right);
+	operator float*()
+	{
+		return &x;
+	}
+
+	float length()
+	{
+		return sqrt((this->x*this->x)+(this->y*this->y));
+	}
+	Vector2D perpCW()
+	{
+		//change x
+		return Vector2D(y, x*-1);
+	}
+	Vector2D perpCCW()
+	{
+		return Vector2D(y*-1, x);
+	}
+	Vector2D normalized()
+	{
+		return Vector2D(1/length() * x, 1/length() * y);
+	}
+};
+
+//std::ostream& operator<<(std::ostream& stream, const Vector& right)
+//{
+//	cout << "{" << right.x << ", " << right.y << "}";
+//	return stream;
+//}
+
+
+Vector2D operator+(const Vector2D& left, const Vector2D& right)
+{
+	return Vector2D(left.x + right.x, left.y + right.y);
+}
+
+Vector2D operator-(const Vector2D& left, const Vector2D& right)
+{
+	return Vector2D(left.x + (-1*right.x), left.y + (-1*right.y));
+}
+
+Vector2D operator*(float scalar, const Vector2D& right)
+{
+	return Vector2D(scalar * right.x, scalar * right.y);
+}
+
+Vector2D operator*(const Vector2D& left, float scalar)
+{
+	return Vector2D(left.x * scalar, left.y * scalar);
+}
+
+Vector2D operator/(const Vector2D& left, float scalar)
+{
+	return Vector2D(left.x/scalar, left.y/scalar);
+}
+
+float dot(const Vector2D& left, const Vector2D& right)
+{
+	return left.x*right.x + left.y*right.y;
+}
+#endif
--- GameSkeleton/GameSkeleton/TwoDCalculator/EntryPoint.cpp
+++ GameSkeleton/GameSkeleton/TwoDCalculator/EntryPoint.cpp
@@ -1,5 +1,61 @@
 #include "RenderUI.h"
 #include "Engine.h"
+#include <Vector2D.h>
+
+Vector2D left;
+Vector2D right;
+Vector2D result;
+
+Vector2D original;
+Vector2D normal;
+Vector2D cwPerp;
+Vector2D ccwPerp;
+
+Vector2D vector1;
+Vector2D vector2;
+Vector2D projectionVector;
+Vector2D rejectionVector;
+
+void MyBasicVectorEquationCallback(const BasicVectorEquationInfo& info)
+{
+	left = info.scalar1 * Vector2D(info.x1, info.y1);
+	right = info.scalar2 * Vector2D(info.x2, info.y2);
+
+	if(info.add)
+	{
+		result = left + right;
+	}
+	else
+	{
+		result = left - right;
+	}
+	
+}
+
+void MyPerpendicularDataCallback(const PerpendicularData& data)
+{
+	original = Vector2D(data.x, data.y);
+	normal = original.normalized();
+	cwPerp = original.perpCW();
+	ccwPerp = original.perpCCW();
+}
+
+void MyDotProductDataCollback(const DotProductData& data)
+{
+	vector1 = Vector2D(data.v1i, data.v1j);
+	vector2 = Vector2D(data.v2i, data.v2j);
+	if(data.projectOntoLeftVector)
+	{
+		projectionVector = dot(vector2, vector1.normalized())*vector1.normalized();
+		rejectionVector = vector2 - projectionVector;
+	}
+	else
+	{
+		projectionVector = dot(vector1, vector2.normalized())*vector2.normalized();
+		rejectionVector = vector1 - projectionVector;
+	}
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -7,6 +63,17 @@ int main(int argc, char* argv[])
 
 	RenderUI renderUI;
 
+	renderUI.setBasicVectorEquationData(
+		MyBasicVectorEquationCallback,
+		left, right, result);
+
+	renderUI.setPerpendicularData(
+		original, normal, cwPerp, ccwPerp, 
+		MyPerpendicularDataCallback);
+
+	renderUI.setDotProductData(
+		vector1, vector2, projectionVector, rejectionVector, 
+		MyDotProductDataCollback);
 	if( ! renderUI.initialize(argc, argv))
 		return -1;
 	return renderUI.run();

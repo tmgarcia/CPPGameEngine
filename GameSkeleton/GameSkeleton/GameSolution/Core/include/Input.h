0dfc6b5ed5c3b25670308630489423fe7cfee626
tmgarcia <tclustuff@gmail.com>
2013-11-01 18:24:20 +0000

    Mapped the Alphabet keys in Winuser and assigned the WASD keys in input, changed SpaceShip Controls to WASD keys instead of arrows, started toggling between bouncing and wrapping
    git-svn-id: https://svn.neumont.edu:8443/svn/taylorgarciarepspot@8 9283bf75-a890-104e-bf60-28af5b2e9440

--- GameSkeleton/GameSkeleton/GameSolution/Core/include/Input.h
+++ GameSkeleton/GameSkeleton/GameSolution/Core/include/Input.h
@@ -9,14 +9,20 @@ namespace Core
 class CORE_SHARED Input
 {
 public:
-
+	
+	static const int KEY_W = VK_W;
+	static const int KEY_A = VK_A;
+	static const int KEY_S = VK_S;
+	static const int KEY_D = VK_D;
 	static const int KEY_LEFT	= VK_LEFT;
 	static const int KEY_RIGHT	= VK_RIGHT;
 	static const int KEY_UP		= VK_UP;
 	static const int KEY_DOWN	= VK_DOWN;
 	static const int KEY_ESCAPE	= VK_ESCAPE;
+	static const int KEY_TAB = VK_TAB;
 	static const int BUTTON_LEFT	= VK_LBUTTON;
 	static const int BUTTON_RIGHT	= VK_RBUTTON;
+
 
 	static bool IsPressed( int key );
 	static void GetMousePos( int& x, int& y );

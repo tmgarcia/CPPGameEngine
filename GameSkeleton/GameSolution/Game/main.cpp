83228e1435b04f4cfa82404b438efecf914fe2be
tmgarcia <tclustuff@gmail.com>
2013-11-20 23:31:00 +0000

    Cleaned up Main
    git-svn-id: https://svn.neumont.edu:8443/svn/taylorgarciarepspot@29 9283bf75-a890-104e-bf60-28af5b2e9440

--- GameSkeleton/GameSolution/Game/main.cpp
+++ GameSkeleton/GameSolution/Game/main.cpp
@@ -4,28 +4,21 @@
 #include "ScreenInfo.h"
 #include "Random.h"
 #include "ParticleEffect.h"
-//using Core::Graphics;
+
 using Core::RGB;
 
-
-
-//ParticleEffect troll(Vector3D(0,1), RGB(0, 0, 255),200);
-
 SpaceShip meShip;
+
 bool Update( float dt)
 {
 	if( Core::Input::IsPressed( Core::Input::KEY_ESCAPE ) )
 		return true;
-//	troll.update(dt);
 	meShip.update(dt);
 	return false;
 }
+
 void Draw( Core::Graphics& graphics)
 {
-//	troll.draw(graphics);
-	//graphics.SetColor( RGB(255, 255, 0) );
-	//graphics.DrawString( SCREEN_WIDTH/2 - 50, SCREEN_HEIGHT/2 - 20, "Hello World");
-	//graphics.DrawLine(10,10,400,300);
 	meShip.draw(graphics);
 }
 
@@ -36,6 +29,5 @@ int main()
 	Core::RegisterUpdateFn( Update);
 	Core::RegisterDrawFn( Draw );
 	Core::GameLoop();
-//	Engine::sampleFunctionThatReturnsTrue();
 }
 

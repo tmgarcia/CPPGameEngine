dbc4afea399bc3c1759e7c0e6f513ee9bf68dd32
tmgarcia <tclustuff@gmail.com>
2014-01-22 23:59:43 +0000

    3D cube added, rotates according to wasdqe keys
    git-svn-id: https://svn.neumont.edu:8443/svn/taylorgarciarepspot@68 9283bf75-a890-104e-bf60-28af5b2e9440

--- GameSkeleton/OpenGLScratchPad/CoolGLWindow.cpp
+++ GameSkeleton/OpenGLScratchPad/CoolGLWindow.cpp
@@ -19,26 +19,9 @@ using glm::mat4;
 
 GLint frameCount = 0;
 GLuint programID;
-
-mat3 p1Transform;
-GLfloat p1Angle = 0;
-vec3 p1Position(-0.5f,0,1);
-vec3 p1Color(0.5f,0.0f,0.8f);
-GLfloat p1ZAdjust;
-GLint p1Points=0;
-
-mat3 p2Transform;
-GLfloat p2Angle = 0;
-vec3 p2Position(0.0f,0,1);
-vec3 p2Color(0.0f,0.5f,0.2f);
-GLfloat p2ZAdjust;
-GLint p2Points=0;
-
-mat3 hillTransform;
-vec3 hillPosition(0,0,1);
-vec3 hillColor(0.0f, 0.0f, 0.0f);
-GLfloat hillZAdjust = 0.0f;
-
+GLfloat xAngle=0.0f;
+GLfloat yAngle=0.0f;
+GLfloat zAngle=0.0f;
 
 void CoolGLWindow::initializeGL()
 {
@@ -54,13 +37,13 @@ void CoolGLWindow::sendDataToHardware()
 	GLuint vertexBufferID;
 	glGenBuffers(1, &vertexBufferID);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Shape::vertices), Shape::vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Shape::verts), Shape::verts, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0,4,GL_FLOAT, GL_FALSE, 2 * sizeof(vec4), 0);
+	glVertexAttribPointer(0,3,GL_FLOAT, GL_FALSE, 2 * sizeof(vec3), 0);
 
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1,4,GL_FLOAT, GL_FALSE, 2 * sizeof(vec4), (void*)(1 * sizeof(vec4)));
+	glVertexAttribPointer(1,3,GL_FLOAT, GL_FALSE, 2 * sizeof(vec3), (void*)(1 * sizeof(vec3)));
 	
 	GLuint indexBufferID;
 	glGenBuffers(1, &indexBufferID);
@@ -73,104 +56,32 @@ void CoolGLWindow::sendDataToHardware()
 
 void CoolGLWindow::myUpdate()
 {
-	float velocityScale = 0.05f;
-	vec3 acc = vec3(0, velocityScale, 0);
-	vec3 direction = mat3(glm::rotate(glm::degrees(-p1Angle),vec3(0,0,1)))*acc;
-	//Player 1 Controls
 	if(GetAsyncKeyState(0x41)<0)//A
 	{
-		p1Angle -= 0.09f;
+		yAngle -= 0.09f;
 	}
 	if(GetAsyncKeyState(0x44)<0)//D
 	{
-		p1Angle += 0.09f;
+		yAngle += 0.09f;
 	}
 	if(GetAsyncKeyState(0x57)<0)//W
 	{
-		p1Position = p1Position + direction;
+		xAngle += 0.09f;
 	}
 	if(GetAsyncKeyState(0x53)<0)//S
 	{
-		p1Position = p1Position - direction;
+		xAngle -= 0.09f;
+	}
+	if(GetAsyncKeyState(0x45)<0)//E
+	{
+		zAngle += 0.09f;
+	}
+	if(GetAsyncKeyState(0x51)<0)//Q
+	{
+		zAngle -= 0.09f;
 	}
 
-	//Player 2 Controls
-	direction = mat3(glm::rotate(glm::degrees(-p2Angle),vec3(0,0,1)))*acc;
-	if(GetAsyncKeyState(VK_LEFT)<0)
-	{
-		p2Angle -= 0.09f;
-	}
-	if(GetAsyncKeyState(VK_RIGHT)<0)
-	{
-		p2Angle += 0.09f;
-	}
-	if(GetAsyncKeyState(VK_UP)<0)
-	{
-		p2Position = p2Position + direction;
-	}
-	if(GetAsyncKeyState(VK_DOWN)<0)
-	{
-		p2Position = p2Position - direction;
-	}
-	
-	updatePlayer(p1Position, p1Angle, true);
-	updatePlayer(p2Position, p2Angle, false);
-	if(frameCount==0 || frameCount % 500==0)
-		updateHill();
-	frameCount++;
 	repaint();
-}
-
-void CoolGLWindow::updatePlayer(vec3 position, GLfloat angle, bool isP1)
-{
-	mat3 temp;
-	mat3 scalar = mat3(glm::scale(glm::mat4(), vec3(0.3f,0.3f,0)));
-	mat3 rotator = mat3(glm::rotate(glm::degrees(angle),vec3(0,0,1)));
-	temp = scalar * rotator;
-	temp[0].z = position.x;
-	temp[1].z = position.y;
-	temp[2].z = 1.0f;
-	float buffer = 0.15f;
-	bool addPoints= (position.x<(hillPosition.x+buffer) && position.x>(hillPosition.x-buffer) &&position.y<(hillPosition.y+buffer) && position.y>(hillPosition.y-buffer));
-	
-	if(isP1)
-	{
-		p1Transform = mat3();
-		p1Transform = p1Transform * temp;
-		if(addPoints)
-			p1Points++;
-		p1ZAdjust = (p1Points>p2Points)? 0.0f: +0.1f;
-	}
-	else
-	{
-		p2Transform = mat3();
-		p2Transform = p2Transform * temp;
-		if(addPoints)
-		{
-			p2Points++;
-		}
-		p2ZAdjust = (p2Points>p1Points)? 0.0f: +0.1f;
-	}
-	
-}
-
-
-void CoolGLWindow::updateHill()
-{
-	//((float)rand()/RAND_MAX) * (max*(min+1))+min;
-	float angle = ((float)rand()/RAND_MAX) * (6.28*(0+1))+0;
-	hillPosition.x = (((float)rand()/RAND_MAX)*2) -1;
-	hillPosition.y = (((float)rand()/RAND_MAX)*2) -1;
-
-	mat3 temp;
-	mat3 scalar = mat3(glm::scale(glm::mat4(), vec3(0.4f,0.4f,0)));
-	mat3 rotator = mat3(glm::rotate(glm::degrees(angle),vec3(0,0,1)));
-	temp = scalar * rotator;
-	temp[0].z = hillPosition.x;
-	temp[1].z = hillPosition.y;
-	temp[2].z = 1.0f;
-	hillTransform = mat3();
-	hillTransform = hillTransform * temp;
 }
 
 std::string CoolGLWindow::readShaderCode(const char *filename)
@@ -226,36 +137,20 @@ void CoolGLWindow::compileShaders()
 
 void CoolGLWindow::paintGL()
 {
+	//glm::perspective(fieldOfView, apsectRatio, Znear, Zfar);
 	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 	glViewport(0,0,width(), height());
-	GLfloat zAdjustment;
-	GLint zAdjustmentLocation = glGetUniformLocation(programID, "zAdjustment");
-	vec3 dominatingColor;
-	GLint dominatingColorLocation = glGetUniformLocation(programID, "dominatingColor");
-	mat3 currentTransform;
+
+	mat4 currentTransform = glm::translate(mat4(), vec3(0.0f, 0.0f, -4.0f));
+	currentTransform *= glm::rotate(glm::degrees(xAngle), vec3(1,0,0));
+	currentTransform *= glm::rotate(glm::degrees(yAngle), vec3(0,1,0));
+	currentTransform *= glm::rotate(glm::degrees(zAngle), vec3(0,0,1));
 	GLint currentTransformLocation = glGetUniformLocation(programID, "currentTransform");
 
-	dominatingColor = p1Color;
-	glUniform3fv(dominatingColorLocation, 1, &(dominatingColor[0]));
-	currentTransform = p1Transform;
-	glUniformMatrix3fv(currentTransformLocation, 1, false, &(currentTransform[0][0]));
-	zAdjustment = p1ZAdjust;
-	glUniform1f(zAdjustmentLocation, zAdjustment);
-	glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_SHORT, 0);
+	mat4 currentProjection = glm::perspective(60.0f, ((float)width()) / width(), 0.1f, 10.0f);
+	GLint currentProjectionLocation = glGetUniformLocation(programID, "currentProjection");
 
-	dominatingColor = p2Color;
-	glUniform3fv(dominatingColorLocation, 1, &(dominatingColor[0]));
-	currentTransform = p2Transform;
-	glUniformMatrix3fv(currentTransformLocation, 1, false, &(currentTransform[0][0]));
-	zAdjustment = p2ZAdjust;
-	glUniform1f(zAdjustmentLocation, zAdjustment);
-	glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_SHORT, 0);
-
-	dominatingColor = hillColor;
-	glUniform3fv(dominatingColorLocation, 1, &(dominatingColor[0]));
-	currentTransform = hillTransform;
-	glUniformMatrix3fv(currentTransformLocation, 1, false, &(currentTransform[0][0]));
-	zAdjustment = hillZAdjust;
-	glUniform1f(zAdjustmentLocation, zAdjustment);
-	glDrawElements(GL_TRIANGLES, 15, GL_UNSIGNED_SHORT, (void*)(24 * sizeof(GLushort)));
+	glUniformMatrix4fv(currentTransformLocation, 1, GL_FALSE, &(currentTransform[0][0]));
+	glUniformMatrix4fv(currentProjectionLocation, 1, GL_FALSE, &(currentProjection[0][0]));
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, 0);
 }
--- GameSkeleton/OpenGLScratchPad/VertexShaderCode.glsl
+++ GameSkeleton/OpenGLScratchPad/VertexShaderCode.glsl
@@ -1,19 +1,18 @@
 #version 400 
  
-in layout(location=0) vec4 position; 
+in layout(location=0) vec3 position; 
 in layout(location=1) vec4 color; 
  
-uniform mat3 currentTransform; 
-uniform vec3 dominatingColor; 
-uniform float zAdjustment; 
+uniform mat4 currentTransform; 
+uniform mat4 currentProjection;
  
 out vec4 deColor; 
  
 void main() 
 { 
-	gl_Position = position; 
-	gl_Position = gl_Position*mat4(currentTransform); 
-	gl_Position.z = gl_Position.z + zAdjustment; 
+	vec4 v = vec4(position,1.0f); 
+	vec4 newPosition = currentTransform * v; 
+	vec4 projectedPosition = currentProjection * newPosition;
+	gl_Position = projectedPosition;
 	deColor = color; 
-	deColor.z = dominatingColor.z; 
 } 
